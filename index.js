e6958ba9f9dcfe5e557732ed058bee2cbb278072
Inspector Soviet <www.alex.avil@mail.ru>
2020-04-02 17:46:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ client.user.setPresence({
 client.on('message', message => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
+  if (message.isMentioned(client.user)) {
+    const mention_responses = [
+      'My relationship with Arle? Can you handle the knowledge?',
+      'I look like Arle? Well of course I do... Haha.',
+      'Now...take me to more fun places.',
+      'Hahaha... You look surprised. Something wrong?',
+      "Ahahaha! I'm having such a great time.",
+      "It feels like I've become stronger. I have to thank you.",
+      "Defeating me... i'll teach you just what that means!",
+      "Hah... what a pointless question... I'm Arle! ...I'm not anything besides that!!",
+    ];
+    message.reply(mention_responses[Math.floor(Math.random() * mention_responses.length)]);
+  };
+
   const args = message.content.slice(config.prefix.length).split(' ');
   const commandName = args.shift().toLowerCase();
   if (!client.commands.has(commandName)) return;
@@ -40,19 +54,6 @@ try {
 	message.reply('there was an error trying to execute that command!');
 }
 
-if (message.isMentioned(client.user)) {
-  const mention_responses = [
-    'My relationship with Arle? Can you handle the knowledge?',
-    'I look like Arle? Well of course I do... Haha.',
-    'Now...take me to more fun places.',
-    'Hahaha... You look surprised. Something wrong?',
-    "Ahahaha! I'm having such a great time.",
-    "It feels like I've become stronger. I have to thank you.",
-    "Defeating me... i'll teach you just what that means!",
-    "Hah... what a pointless question... I'm Arle! ...I'm not anything besides that!!",
-  ];
-  message.reply(mention_responses[Math.floor(Math.random() * mention_responses.length)]);
-}
 });
 
 client.login(process.env.bot_token);

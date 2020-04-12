b0a8400c3eebebd7e6b778536ff8a3b9ca953f91
Inspector Soviet <www.alex.avil@mail.ru>
2020-04-12 20:13:17 +0400

    Update spell.js

--- commands/spell.js
+++ commands/spell.js
@@ -6,12 +6,12 @@ module.exports = {
 
     const chants = [
       'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/abyss.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/chaos.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/eclipse.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/grandcross.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/labyrinth.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/ragnarok.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/voidhole.wav',     
+      'https://cdn.discordapp.com/attachments/694943149142966396/697864042902978650/chaos.wav',
+      'https://cdn.discordapp.com/attachments/694943149142966396/697864045977403452/eclipse.wav',
+      'https://cdn.discordapp.com/attachments/694943149142966396/697864046611005561/grandcross.wav',
+      'https://cdn.discordapp.com/attachments/694943149142966396/697864047546204190/labyrinth.wav',
+      'https://cdn.discordapp.com/attachments/694943149142966396/697864048909353064/ragnarok.wav',
+      'https://cdn.discordapp.com/attachments/694943149142966396/697864050767298700/voidhole.wav',     
 ];
         
 message.channel.send("I am strong... Which should prove useful to you. That should suffice, no?", {

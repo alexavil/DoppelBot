d732631020286d80b16c4556f40096023ca00d48
Inspector Soviet <www.alex.avil@mail.ru>
2020-06-15 14:33:57 +0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -4,6 +4,6 @@ module.exports = {
   description: 'About the bot',
 	execute(message) {
         
-message.channel.send(`Hi, I'm DoppelBot! :heart:\nIf you need a random picture of Doppelganger Arie, I can help you with that.\nUse >doppel to start the magic.\n\nYou can also use >doppelfact to get a random fact about Doppel, >spell if you want to hear a spell chant and >spelldesc to read a spell's description.\nMention me and I will respond with Doppel's quotes! :wink:\n\nA Doppel a day keeps your sadness away! :smile:`)
+message.channel.send(`Hi, I'm DoppelBot! :heart:\nIf you need a random picture of Doppelganger Arie, I can help you with that.\nUse d!doppel to start the magic.\n\nYou can also use d!doppelfact to get a random fact about Doppel, d!spell if you want to hear a spell chant and d!spelldesc to read a spell's description.\nMention me and I will respond with Doppel's quotes! :wink:\n\nA Doppel a day keeps your sadness away! :smile:`)
 	},
 };

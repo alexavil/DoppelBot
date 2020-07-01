a12c2c7c41d6db526481404124ad14faf134e871
Inspector Soviet <www.alex.avil@mail.ru>
2020-07-01 21:38:51 +0400

    Update doppelfact.js

--- commands/doppelfact.js
+++ commands/doppelfact.js
@@ -10,6 +10,7 @@ module.exports = {
       ":information_source: | It is heavily implied that Doppel's spells are more powerful than Arle's, as Void Hole (Doppel's version of Ruipanko) lasts twice as long. ",
       ":information_source: | It's rather unknown if Doppel died or not after the events of Puyo Puyo 4. As it is implied that she did indeed die after her defeat, but in the credits scene, we see her overlooking the Puyo Puyo circus as herself (and not as Pierrot), implying that she's alright.",
       ":information_source: | According to the Puyo Puyo canon, Doppel spent 500 years on the void, implying that the Madou Monogotari canon is canon here too, as Arle (Pre-Reset) fought the creator and won. However, that victory came at the cost of everything as she knew: it all turned into a void. The Arle that we know today is actually half of this original Arle. The other half would be Doppel, which was stuck in the void untill the events of Puyo Puyo 4.",
+      ":information_source: | Rumor has it that Doppel can take people to the Unknown Dimension. It looks like a void similar to the one she spent 500 years in, and there's no way out of it except letting someone drag you out of it, which currently doesn't work 100% of the time. The Unknown Dimension may also appear on well-known locations, which makes it more dangerous.",
     ];
         
 message.channel.send(facts[Math.floor(Math.random() * facts.length)]);

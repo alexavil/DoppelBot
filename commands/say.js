fa10e4e4bdd888bdad6597ccfe1227683169ce0d
Inspector Soviet <www.alex.avil@mail.ru>
2020-11-19 13:50:21 +0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'say',
   description: 'Make the bot say something!',
-	execute(message) {
+	execute(message, args) {
   if (message.author.id === '332148103803174913') {
   let chl = message.mentions.channels.first();
   let msg = args.slice(1).join(" ");

e3ae1177a9688207eb8830345729f1003f0b3ef1
Inspector Soviet <www.alex.avil@mail.ru>
2020-12-14 21:45:17 +0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { Client, RichEmbed, Permissions, PermissionOverwrites, GuildMember, } = require('discord.js');
 module.exports = {
 	name: 'about',
   description: 'About the bot',

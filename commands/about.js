bc94a20b4f22a0575c230fae7ae30e9c77229305
Inspector Soviet <www.alex.avil@mail.ru>
2020-12-14 21:36:47 +0400

    prototype about

--- commands/about.js
+++ commands/about.js
@@ -2,9 +2,16 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'about',
   description: 'About the bot',
-	execute(message) {
-        
-message.channel.send(`Hi, I'm DoppelBot! :heart:\nIf you need a random picture of Doppelganger Arie, I can help you with that.\nUse d!doppel to start the magic.\n\nYou can also use d!doppelfact to get a random fact about Doppel, d!spell if you want to hear a spell chant and d!spelldesc to read a spell's description.\nMention me and I will respond with Doppel's quotes! :wink:\n\nA Doppel a day keeps your sadness away! :smile:`);
-message.delete().catch();
+	 execute(message) {
+        const help = new Discord.MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle("Hi, I'm DoppelBot! :heart:")
+		.setDescription('')
+		.addFields(
+			{ name: 'Info', value: "If you need a random picture of Doppelganger Arie, I can help you with that. Use d!doppel to start the magic.\n\nd!doppelfact - get a random fact about Doppel\nd!spell - hear a spell chant\nd!spelldesc - read a spell description\n\nMention me and I will respond with Doppel's quotes! :wink:\n\nA Doppel a day keeps your sadness away! :smile:" },
+		)
+		.setTimestamp()
+	    message.channel.send(help);
+		message.delete().catch();
 	},
 };

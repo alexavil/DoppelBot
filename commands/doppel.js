7a9d0bab6477fd22da457467c5dc7d38c269243d
Inspector Soviet <www.alex.avil@mail.ru>
2020-12-15 16:22:23 +0400

    Update doppel.js

--- commands/doppel.js
+++ commands/doppel.js
@@ -3,8 +3,7 @@ module.exports = {
 	name: 'doppel',
   description: 'Get a random picture of Doppel',
 	execute(message) {
-		
-		message.delete().catch();
+	
 
     function randomInt(min, max) {
       return min + Math.floor((max - min) * Math.random());
@@ -193,16 +192,19 @@ if (chance == 58) {
   message.channel.send("Here's your... wait, this isn't Doppel... Could anything ELSE go wrong today?!?!", {
     file: "./images/mq2.png"
   });
+  message.delete().catch();
 }
 if (chance == 27) {
   message.channel.send("Here's your... wait, this isn't Doppel... it's Boreas! At this time of year, at this time of day, in this part of the country, localized entirely within this channel?!", {
     file: "./images/borealis.png"
   });
+	  message.delete().catch();
 }
 if ((chance != 58) && (chance != 27)) {
 message.channel.send(responses[Math.floor(Math.random() * responses.length)], {
   file: doppel_imgs[Math.floor(Math.random() * doppel_imgs.length)]
 });
+	  message.delete().catch();
 }
 	},
 };

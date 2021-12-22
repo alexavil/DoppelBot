3f437961f5b468ac0e33d1d9528c1fc4103d299d
Inspector Soviet <www.alex.avil@mail.ru>
2021-12-22 23:27:47 +0400

    sqlite3 - about

--- commands/about.js
+++ commands/about.js
@@ -1,20 +1,30 @@
 const Discord = require('discord.js');
-const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
 module.exports = {
 	name: 'about',
 	aliases: ['help'],
   description: 'About the bot',
 	 execute(message) {
-	id = message.guild.id;
-	const guildconf = JSON.parse(fs.readFileSync('./guilds/' + id + '.json'));
-        const help = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle("Hi, I'm DoppelBot! :heart:")
-	.addField('How to use', 'If you need a random picture of Doppelganger Arle, I can help you with that. Use ' + guildconf.prefix + 'doppel to start the magic.')
-	.addField('What else can I do?', "You can also use " + guildconf.prefix + "doppelfact to get a random fact about Doppel, " + guildconf.prefix + "spell if you want to hear a spell chant and " + guildconf.prefix + "spelldesc to read a spell description. Mention me and I will respond with Doppel's quotes! :wink:")
-	.addField('Party commands', guildconf.prefix + "createparty - create a party\n" + guildconf.prefix + "joinparty - join a party by code\n" + guildconf.prefix + "leaveparty - leave a party\n" + guildconf.prefix + "endparty - destroy a party\n" + guildconf.prefix + "makeleader - transfer leadership")
-	.addField('Music commands (WIP)', guildconf.prefix + "play (p) - play music\n" + guildconf.prefix + "search - search for music\n" + guildconf.prefix + "stop - stop playing")
-	.setFooter('To view administrator commands, use ' + guildconf.prefix + 'admhelp')
-    message.channel.send({ embeds: [help] });
+		 id = message.guild.id;
+		 let settings = new sqlite3.Database('./guilds.db', (err) => {
+			 if (err) {
+				 console.error(err.message);
+			 }
+			 console.log('Connected to the settings database.');
+		 });
+		 settings.get('SELECT prefix FROM guilds WHERE id=?', [id], (err, row) => {
+			 if (err) {
+				 throw err;
+			 }
+			 const help = new Discord.MessageEmbed()
+				 .setColor('#0099ff')
+				 .setTitle("Hi, I'm DoppelBot! :heart:")
+				 .addField('How to use', 'If you need a random picture of Doppelganger Arle, I can help you with that. Use ' + row.prefix + 'doppel to start the magic.')
+				 .addField('What else can I do?', "You can also use " + row.prefix + "doppelfact to get a random fact about Doppel, " + row.prefix + "spell if you want to hear a spell chant and " + row.prefix + "spelldesc to read a spell description. Mention me and I will respond with Doppel's quotes! :wink:")
+				 .addField('Party commands', row.prefix + "createparty - create a party\n" + row.prefix + "joinparty - join a party by code\n" + row.prefix + "leaveparty - leave a party\n" + row.prefix + "endparty - destroy a party\n" + row.prefix + "makeleader - transfer leadership")
+				 .addField('Music commands (WIP)', row.prefix + "play (p) - play music\n" + row.prefix + "search - search for music\n" + row.prefix + "stop - stop playing")
+				 .setFooter('To view administrator commands, use ' + row.prefix + 'admhelp')
+			 message.channel.send({ embeds: [help] });
+		 });
 	},
 };

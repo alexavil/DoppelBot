d6fb9a8944b08f63d84b4cc402fc2607fb798474
Inspector Soviet <www.alex.avil@mail.ru>
2021-12-22 23:32:24 +0400

    sqlite3 - admhelp

--- commands/admhelp.js
+++ commands/admhelp.js
@@ -1,17 +1,27 @@
 const Discord = require('discord.js');
-const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
 module.exports = {
 	name: 'admhelp',
   description: 'About the bot - for admins',
 	 execute(message) {
 	id = message.guild.id;
-	const guildconf = JSON.parse(fs.readFileSync('./guilds/' + id + '.json'));
-        const help = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle("DoppelBot's Administrator commands")
-	.addField('Moderation commands', guildconf.prefix + "ban - ban a user\n" + guildconf.prefix + "unban - unban a user\n" + guildconf.prefix + "warn - issue a warning\n" + guildconf.prefix + "kick - kick a user")
-	.addField('Server settings', guildconf.prefix + "settings - view settings\n" + guildconf.prefix + "toggleaa (aa) - toggle Ace Attorney images\n" + guildconf.prefix + "togglementions (mentions) - toggle mention responses\n" + guildconf.prefix + "toggleother (other) - toggle other responses\n" + guildconf.prefix + "setprefix (prefix) - set guild prefix\n" + guildconf.prefix + "togglefilter (filter) - toggle word filter\n" + guildconf.prefix + "togglegb (gb) - toggle global bans")
-	.addField('Filter management', guildconf.prefix + "filteradd - add words to filter (accepts multiple arguments)\n" + guildconf.prefix + "filterremove - remove words from filter (accepts multiple arguments)\n" + guildconf.prefix + "viewfilter - view current filter")
-    message.channel.send({ embeds: [help] });
+		 let settings = new sqlite3.Database('./guilds.db', (err) => {
+			 if (err) {
+				 console.error(err.message);
+			 }
+			 console.log('Connected to the settings database.');
+		 });
+		 settings.get('SELECT prefix FROM guilds WHERE id=?', [id], (err, row) => {
+			 if (err) {
+				 throw err;
+			 }
+			 const help = new Discord.MessageEmbed()
+				 .setColor('#0099ff')
+				 .setTitle("DoppelBot's Administrator commands")
+				 .addField('Moderation commands', row.prefix + "ban - ban a user\n" + row.prefix + "unban - unban a user\n" + row.prefix + "warn - issue a warning\n" + row.prefix + "kick - kick a user")
+				 .addField('Server settings', row.prefix + "settings - view settings\n" + row.prefix + "toggleaa (aa) - toggle Ace Attorney images\n" + row.prefix + "togglementions (mentions) - toggle mention responses\n" + row.prefix + "toggleother (other) - toggle other responses\n" + row.prefix + "setprefix (prefix) - set guild prefix\n" + row.prefix + "togglefilter (filter) - toggle word filter\n" + row.prefix + "togglegb (gb) - toggle global bans")
+				 .addField('Filter management', row.prefix + "filteradd - add words to filter (accepts multiple arguments)\n" + row.prefix + "filterremove - remove words from filter (accepts multiple arguments)\n" + row.prefix + "viewfilter - view current filter")
+			 message.channel.send({ embeds: [help] });
+		 });   
 	},
 };

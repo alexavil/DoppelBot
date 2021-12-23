a961c84973b524f29202ebb05e3a01c71e5b1452
Inspector Soviet <www.alex.avil@mail.ru>
2021-12-23 17:38:27 +0400

    sqlite3 - setprefix

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,29 +1,27 @@
 const Discord = require('discord.js');
-const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
 module.exports = {
 	name: 'setprefix',
 	aliases: ['prefix'],
   description: 'Set guild prefix',
   userpermissions: 'BAN_MEMBERS',
-	 execute(message, args) {
+	execute(message, args) {
+		if (!args.length) {
+			message.reply("Please provide a prefix!");
+		}
 		 id = message.guild.id;
-	const guildconf = JSON.parse(fs.readFileSync('./guilds/' + id + '.json'));
-	console.log(guildconf);
-	if (!args.length) {
-		message.reply("Please provide a prefix!");
-	}
-		var stream = fs.createWriteStream('./guilds/' + id + '.json');
-		stream.once('open', (fd) => {
-		stream.write("{\n");
-		stream.write(`"aa": "` + guildconf.aa +`",\n`);
-		stream.write(`"mentions": "` + guildconf.mentions +`",\n`);
-		stream.write(`"other": "` + guildconf.other +`",\n`);
-		stream.write(`"prefix": "` + args[0] +`",\n`);
-		stream.write(`"filter": "` + guildconf.filter +`",\n`);
-		stream.write(`"global_bans": "` + guildconf.global_bans +`"\n`);		
-		stream.write("}");
-		stream.end();
-});
-	message.reply('New prefix set to ' + args[0] + '.');
+		 let settings = new sqlite3.Database('./guilds.db', (err) => {
+			 if (err) {
+				 console.error(err.message);
+			 }
+			 console.log('Connected to the settings database.');
+		 });
+		settings.run(`UPDATE guilds SET prefix=? WHERE id=?`, [args[0], id], function (err) {
+			if (err) {
+				return console.log(err.message);
+			}
+			message.reply('New prefix set to ' + args[0] + '.');
+		});
+	
 	},
 };

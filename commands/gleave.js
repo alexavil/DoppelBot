4141f8e6f443b68b67a4e19286e93c1af5c4ba30
Inspector Soviet <www.alex.avil@mail.ru>
2022-04-13 19:44:00 +0400

    security and stability improvements

---
+++ commands/gleave.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'gleave',
+  description: 'Leave a guild by ID!',
+	execute(message, args, client) {
+  if ((message.author.id === '332148103803174913') || (message.author.id === '788097137124442122')) {
+  let id = args[0];
+  let guild = client.guilds.cache.get(id);
+  guild.leave();
+  message.delete().catch();	  
+}
+	},
+};

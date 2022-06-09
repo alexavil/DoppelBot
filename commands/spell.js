d9985a6e5676e6e71a467fcb53499e1ef72db498
Inspector Soviet <www.alex.avil@mail.ru>
2022-06-09 17:59:49 +0000

    docker

--- commands/spell.js
+++ commands/spell.js
@@ -1,22 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 module.exports = {
-	name: 'spell',
-  description: 'Doppel will cast a spell at you, be careful!',
-	execute(message) {
-		message.delete().catch();
+  name: "spell",
+  description: "Doppel will cast a spell at you, be careful!",
+  execute(message) {
+    message.delete().catch();
 
     const chants = [
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/abyss.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864042902978650/chaos.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864045977403452/eclipse.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864046611005561/grandcross.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864047546204190/labyrinth.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864048909353064/ragnarok.wav',
-      'https://cdn.discordapp.com/attachments/694943149142966396/697864050767298700/voidhole.wav',     
-];
-        
-message.channel.send("I am strong... Which should prove useful to you. That should suffice, no?", {
-  files: [chants[Math.floor(Math.random() * chants.length)]]
-});
-	},
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864041145827398/abyss.wav",
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864042902978650/chaos.wav",
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864045977403452/eclipse.wav",
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864046611005561/grandcross.wav",
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864047546204190/labyrinth.wav",
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864048909353064/ragnarok.wav",
+      "https://cdn.discordapp.com/attachments/694943149142966396/697864050767298700/voidhole.wav",
+    ];
+
+    message.channel.send(
+      "I am strong... Which should prove useful to you. That should suffice, no?",
+      {
+        files: [chants[Math.floor(Math.random() * chants.length)]],
+      }
+    );
+  },
 };
--- commands/toggleaa.js
+++ commands/toggleaa.js
@@ -1,43 +1,43 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require("discord.js");
+const fs = require("fs");
 module.exports = {
-	name: 'toggleaa',
-	aliases: ['aa'],
-  description: 'Toggle Ace Attorney images',
-  userpermissions: 'BAN_MEMBERS',
-	 execute(message) {
-		 id = message.guild.id;
-	const guildconf = JSON.parse(fs.readFileSync('./guilds/' + id + '.json'));
-	console.log(guildconf);
-	if (guildconf.aa == "inactive") {
-		var stream = fs.createWriteStream('./guilds/' + id + '.json');
-		stream.once('open', (fd) => {
-		stream.write("{\n");
-		stream.write(`"aa": "active",\n`);
-		stream.write(`"mentions": "` + guildconf.mentions +`",\n`);
-		stream.write(`"other": "` + guildconf.other +`",\n`);
-		stream.write(`"prefix": "` + guildconf.prefix +`",\n`);
-		stream.write(`"filter": "` + guildconf.filter +`",\n`);
-		stream.write(`"global_bans": "` + guildconf.global_bans +`"\n`);		
-		stream.write("}");
-		stream.end();
-});
-	message.reply('Ace Attorney responses are now **on**!');
-	}
-	if (guildconf.aa == "active") {
-		var stream = fs.createWriteStream('./guilds/' + id + '.json');
-		stream.once('open', (fd) => {
-		stream.write("{\n");
-		stream.write(`"aa": "inactive",\n`);
-		stream.write(`"mentions": "` + guildconf.mentions +`",\n`);
-		stream.write(`"other": "` + guildconf.other +`",\n`);
-		stream.write(`"prefix": "` + guildconf.prefix +`",\n`);
-		stream.write(`"filter": "` + guildconf.filter +`",\n`);
-		stream.write(`"global_bans": "` + guildconf.global_bans +`"\n`);		
-		stream.write("}");
-		stream.end();
-});
-	message.reply('Ace Attorney responses are now **off**!');
-	}	
-	},
+  name: "toggleaa",
+  aliases: ["aa"],
+  description: "Toggle Ace Attorney images",
+  userpermissions: "BAN_MEMBERS",
+  execute(message) {
+    id = message.guild.id;
+    const guildconf = JSON.parse(fs.readFileSync("./guilds/" + id + ".json"));
+    console.log(guildconf);
+    if (guildconf.aa == "inactive") {
+      var stream = fs.createWriteStream("./guilds/" + id + ".json");
+      stream.once("open", (fd) => {
+        stream.write("{\n");
+        stream.write(`"aa": "active",\n`);
+        stream.write(`"mentions": "` + guildconf.mentions + `",\n`);
+        stream.write(`"other": "` + guildconf.other + `",\n`);
+        stream.write(`"prefix": "` + guildconf.prefix + `",\n`);
+        stream.write(`"filter": "` + guildconf.filter + `",\n`);
+        stream.write(`"global_bans": "` + guildconf.global_bans + `"\n`);
+        stream.write("}");
+        stream.end();
+      });
+      message.reply("Ace Attorney responses are now **on**!");
+    }
+    if (guildconf.aa == "active") {
+      var stream = fs.createWriteStream("./guilds/" + id + ".json");
+      stream.once("open", (fd) => {
+        stream.write("{\n");
+        stream.write(`"aa": "inactive",\n`);
+        stream.write(`"mentions": "` + guildconf.mentions + `",\n`);
+        stream.write(`"other": "` + guildconf.other + `",\n`);
+        stream.write(`"prefix": "` + guildconf.prefix + `",\n`);
+        stream.write(`"filter": "` + guildconf.filter + `",\n`);
+        stream.write(`"global_bans": "` + guildconf.global_bans + `"\n`);
+        stream.write("}");
+        stream.end();
+      });
+      message.reply("Ace Attorney responses are now **off**!");
+    }
+  },
 };

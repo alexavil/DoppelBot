8e2dfde4e5548f1cda0aa9bf12639a476d6dabb2
alexavil <www.alex.avil@mail.ru>
2022-11-30 21:46:39 +0400

    Classic DoppelBot: Formatting

--- commands/createparty.js
+++ commands/createparty.js
@@ -1,36 +1,66 @@
-const Discord = require('discord.js');
-const { Client, MessageEmbed, Permissions, PermissionOverwrites, GuildMember, MessageAttachment, Intents } = require('discord.js');
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
+const Discord = require("discord.js");
+const {
+  Client,
+  MessageEmbed,
+  Permissions,
+  PermissionOverwrites,
+  GuildMember,
+  MessageAttachment,
+  Intents,
+} = require("discord.js");
+const client = new Discord.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+});
 module.exports = {
-	name: 'createparty',
-  description: 'Create a party',
-	 execute(message) {
-		function randomInt(min, max) {
-			return min + Math.floor((max - min) * Math.random());
-		  }
-		  id = randomInt(1, 999);
-		  chname = "party-" + id.toString();
-		  code = randomInt(1000, 9999);
-		message.guild.channels.create(chname, {
-			type: 'text',
-			topic: 'Party code: ' + code + ". Leader: " + message.author.tag,
-			permissionOverwrites: [
-				{
-					id: message.guild.id,
-					deny: ['VIEW_CHANNEL'],
-				},
-				{
-					id: message.author.id,
-					allow: ['VIEW_CHANNEL', 'MANAGE_MESSAGES'],
-				},
-			],
-		});
-			message.author.send('Your party (' +  chname + ') code: ' + code + '.').catch(error => {
-				if (error.code === 50007) {
-					console.error('Failed to send the message:', error);
-					return message.author.send('I could not send you the code. Look it up in your new channel topic.');
-				}
-			});
-			message.delete();		
-	},
+  name: "createparty",
+  description: "Create a party",
+  execute(message) {
+    function randomInt(min, max) {
+      return min + Math.floor((max - min) * Math.random());
+    }
+    id = randomInt(1, 999);
+    chname = "party-" + id.toString();
+    code = randomInt(1000, 9999);
+    message.guild.channels.create(chname, {
+      type: "text",
+      topic: "Party code: " + code + ". Leader: " + message.author.tag,
+      permissionOverwrites: [
+        {
+          id: message.guild.id,
+          deny: ["VIEW_CHANNEL"],
+        },
+        {
+          id: message.author.id,
+          allow: ["VIEW_CHANNEL", "MANAGE_MESSAGES"],
+        },
+      ],
+    });
+    message.author
+      .send("Your party (" + chname + ") code: " + code + ".")
+      .catch((error) => {
+        if (error.code === 50007) {
+          console.error("Failed to send the message:", error);
+          return message.author.send(
+            "I could not send you the code. Look it up in your new channel topic."
+          );
+        }
+      });
+    message.delete();
+  },
 };
--- commands/filterremove.js
+++ commands/filterremove.js
@@ -1,56 +1,55 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require("discord.js");
+const fs = require("fs");
 module.exports = {
-	name: 'filterremove',
-  description: 'Manage filter',
-  userpermissions: 'ADMINISTRATOR',
-	 execute(message, args) {
-		 id = message.guild.id;
-	const guildconf = JSON.parse(fs.readFileSync('./filter/' + id + '.json'));
-	console.log(guildconf);
-	if(!args.length) {
-		return message.reply('Provide at least one banned word or string!');
-	}
-	if(args.length == 1) {
-			oldfilter = JSON.parse(fs.readFileSync('./filter/' + id + '.json'));
-			console.log(oldfilter);
-			if (!oldfilter.banned_words.includes(args[0])) {
-				return message.reply("This word doesn't exist!");
-			}
-			var index = oldfilter.banned_words.indexOf(args[0]);			
-			newfilter = oldfilter.banned_words.splice(index, 1);
-			console.log(newfilter);
-			let data = JSON.stringify(oldfilter);
-			fs.writeFileSync('./filter/' + id + '.json', data, (err) => {
-			if (err) {
-				throw err;
-			} else {
-			console.log("item written successfully.");
+  name: "filterremove",
+  description: "Manage filter",
+  userpermissions: "ADMINISTRATOR",
+  execute(message, args) {
+    id = message.guild.id;
+    const guildconf = JSON.parse(fs.readFileSync("./filter/" + id + ".json"));
+    console.log(guildconf);
+    if (!args.length) {
+      return message.reply("Provide at least one banned word or string!");
     }
-  });
-  return message.reply('Removed!');
-	}
-	if(args.length > 1) {
-		args.forEach(item => {
-			oldfilter = JSON.parse(fs.readFileSync('./filter/' + id + '.json'));
-			console.log(oldfilter);
-			if (!oldfilter.banned_words.includes(item)) {
-				return console.log("This word doesn't exist!");
-			}
-			var index = oldfilter.banned_words.indexOf(item);			
-			newfilter = oldfilter.banned_words.splice(index, 1);
-			console.log(newfilter);
-			let data = JSON.stringify(oldfilter);
-			fs.writeFileSync('./filter/' + id + '.json', data, (err) => {
-			if (err) {
-				throw err;
-			} else {
-			console.log("item written successfully.");
+    if (args.length == 1) {
+      oldfilter = JSON.parse(fs.readFileSync("./filter/" + id + ".json"));
+      console.log(oldfilter);
+      if (!oldfilter.banned_words.includes(args[0])) {
+        return message.reply("This word doesn't exist!");
+      }
+      var index = oldfilter.banned_words.indexOf(args[0]);
+      newfilter = oldfilter.banned_words.splice(index, 1);
+      console.log(newfilter);
+      let data = JSON.stringify(oldfilter);
+      fs.writeFileSync("./filter/" + id + ".json", data, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log("item written successfully.");
+        }
+      });
+      return message.reply("Removed!");
     }
-  });
-	});
-	return message.reply('Removed!');
-	}	
-	
-	},
+    if (args.length > 1) {
+      args.forEach((item) => {
+        oldfilter = JSON.parse(fs.readFileSync("./filter/" + id + ".json"));
+        console.log(oldfilter);
+        if (!oldfilter.banned_words.includes(item)) {
+          return console.log("This word doesn't exist!");
+        }
+        var index = oldfilter.banned_words.indexOf(item);
+        newfilter = oldfilter.banned_words.splice(index, 1);
+        console.log(newfilter);
+        let data = JSON.stringify(oldfilter);
+        fs.writeFileSync("./filter/" + id + ".json", data, (err) => {
+          if (err) {
+            throw err;
+          } else {
+            console.log("item written successfully.");
+          }
+        });
+      });
+      return message.reply("Removed!");
+    }
+  },
 };

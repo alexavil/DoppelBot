12cf8602ee36648da087703646fd88ea2a71d451
Inspector Soviet <www.alex.avil@mail.ru>
2023-02-22 12:44:32 +0400

    mention tags in help

--- commands/about.js
+++ commands/about.js
@@ -11,26 +11,31 @@ module.exports = {
       .prepare(`SELECT value FROM guild_${id} WHERE option = 'prefix'`)
       .get().value;
     let version = settings
-      .prepare(
-        `SELECT value FROM global WHERE option = 'current_version'`
-      )
+      .prepare(`SELECT value FROM global WHERE option = 'current_version'`)
       .get().value;
     const help = new Discord.MessageEmbed()
       .setColor("#0099ff")
       .setTitle("Hi, I'm DoppelBot! :heart:")
-      .addFields({
-        name: "Music commands",
-        value:
-          "All music commands start with " +
-          prefix +
-          "music.\n" +
-          "play - play music\n" +
-          "search - search for music\n" +
-          "pause - pause the current track\n" +
-          "stop - stop playing\n" +
-          "queue - view the current queue\n" +
-          "skip - skip the current track.",
-      })
+      .addFields(
+        {
+          name: "Music commands",
+          value:
+            "All music commands start with " +
+            prefix +
+            "music.\n" +
+            "play - play music\n" +
+            "search - search for music\n" +
+            "pause - pause the current track\n" +
+            "stop - stop playing\n" +
+            "queue - view the current queue\n" +
+            "skip - skip the current track.",
+        },
+        {
+          name: "Tags",
+          value:
+            "Certain servers may use key phrases and give out a response! You can view all the tags with " + prefix + "tags list",
+        }
+      )
       .setFooter({ text: `Build: ${version}` });
     message.channel.send({ embeds: [help] });
   },

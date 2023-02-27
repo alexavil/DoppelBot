3a7871a4ce30ac523597f7f28b4937aa1d29a75e
Inspector Soviet <www.alex.avil@mail.ru>
2023-02-28 00:02:57 +0400

    why do I need a channel mention?

--- commands/say.js
+++ commands/say.js
@@ -7,7 +7,6 @@ module.exports = {
       message.author.id === "788097137124442122"
     ) {
       let chl = message.mentions.channels.first();
-      let msg = args.slice(1).join(" ");
       let attachments = [];
       if (message.attachments) {
         console.log(message.attachments);
@@ -17,9 +16,17 @@ module.exports = {
           console.log(attachments);
         });
       }
-      chl.send(msg, {
-        files: attachments,
-      });
+      if (chl) {
+        let msg = args.slice(1).join(" ");
+        chl.send(msg, {
+          files: attachments,
+        });
+      } else {
+        let msg = args.join(" ");
+        message.channel.send(msg, {
+          files: attachments,
+        });
+      }
       message.delete().catch();
     }
   },

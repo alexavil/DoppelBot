0c6d68695277ebd50e6970447adddfd0dbccb3b9
Inspector Soviet <www.alex.avil@mail.ru>
2023-04-02 21:13:33 +0400

    InvidJS: edit new settings

---
+++ commands/sethealth.js
@@ -0,0 +1,19 @@
+const sqlite3 = require("better-sqlite3");
+module.exports = {
+  name: "sethealth",
+  aliases: ["health"],
+  description: "Set minimum Invidious instance health",
+  userpermissions: "BAN_MEMBERS",
+  async execute(message, args) {
+    let id = message.guild.id;
+    let settings = new sqlite3("./data/settings.db");
+    if (!args.length || args.length > 1 || parseFloat(args[0]) < 0) {
+      return message.reply("Please provide a valid number!");
+    }
+    let health = parseFloat(args[0]);
+    settings
+      .prepare(`UPDATE guild_${id} SET value = ? WHERE option = ?`)
+      .run(health, "instance_health_threshold");
+    return message.reply("You will be warned if the instance health is below " +  health + "!");
+  },
+};
---
+++ commands/setinstance.js
@@ -0,0 +1,30 @@
+const sqlite3 = require("better-sqlite3");
+const InvidJS = require("@invidjs/invid-js");
+module.exports = {
+  name: "setinstance",
+  aliases: ["instance"],
+  description: "Set default Invidious Instance",
+  userpermissions: "BAN_MEMBERS",
+  async execute(message, args) {
+    let id = message.guild.id;
+    let settings = new sqlite3("./data/settings.db");
+    if (!args.length || args.length > 1) {
+      return message.reply("Please provide a valid Invidious instance URL!");
+    }
+    let url = args[0];
+    if (url[url.length - 1] === "/") {
+      url.slice(0, -1);
+    }
+    let result = await InvidJS.fetchInstances({ url: url });
+    if (!result.length) {
+      return message.reply("Please provide a valid Invidious instance URL!");
+    }
+    if (result[0].api_allowed  === false) {
+      return message.reply("This instance does not allow API calls!");
+    }
+    settings
+      .prepare(`UPDATE guild_${id} SET value = ? WHERE option = ?`)
+      .run(url, "default_instance");
+    return message.reply("`" + url + "` is now the default instance!");
+  },
+};

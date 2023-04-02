f04bac0256dd8e2c852d6d826ac1a26f2de4b7fa
Inspector Soviet <www.alex.avil@mail.ru>
2023-04-02 21:24:15 +0400

    explain every setting

--- commands/settings.js
+++ commands/settings.js
@@ -13,42 +13,58 @@ module.exports = {
       .addFields(
         {
           name: "**Service Notifications**",
-          value: settings
-            .prepare(`SELECT * FROM guild_${id} WHERE option = 'notifications'`)
-            .get().value,
+          value:
+            "Should the bot send notifications to the Server Owner?\nCurrent value: `" +
+            settings
+              .prepare(
+                `SELECT * FROM guild_${id} WHERE option = 'notifications'`
+              )
+              .get().value +
+            "`",
         },
         {
           name: "**Guild Prefix**",
-          value: settings
-            .prepare(`SELECT * FROM guild_${id} WHERE option = 'prefix'`)
-            .get().value,
+          value:
+            "A prefix for any command executed in this server.\nCurrent value: `" +
+            settings
+              .prepare(`SELECT * FROM guild_${id} WHERE option = 'prefix'`)
+              .get().value +
+            "`",
         },
         {
           name: "**Disconnect Timeout**",
           value:
+            "How long will the bot will idle before disconnecting from a voice channel?\nCurrent value: `" +
             parseInt(
               settings
                 .prepare(
                   `SELECT * FROM guild_${id} WHERE option = 'disconnect_timeout'`
                 )
                 .get().value
-            ) + " seconds",
+            ) +
+            " seconds`",
         },
         {
           name: "**Default Invidious Instance**",
-          value: settings
-            .prepare(
-              `SELECT * FROM guild_${id} WHERE option = 'default_instance'`
-            )
-            .get().value,
+          value:
+            "Default/fallback Invidious instance for the guild. The bot will attempt to connect to this instance if no other instance is specified in the command or if the provided instance is down.\nCurrent value: `" +
+            settings
+              .prepare(
+                `SELECT * FROM guild_${id} WHERE option = 'default_instance'`
+              )
+              .get().value +
+            "`",
         },
         {
           name: "**Instance Health Threshold**",
-          value: settings
-            .prepare(
-              `SELECT * FROM guild_${id} WHERE option = 'instance_health_threshold'`
-            )
-            .get().value,
+          value:
+            "The bot will send a warning if the instance health is below this number.\nCurrent value: `" +
+            settings
+              .prepare(
+                `SELECT * FROM guild_${id} WHERE option = 'instance_health_threshold'`
+              )
+              .get().value +
+            "`",
         }
       );
     message.channel.send({ embeds: [settingsembed] });

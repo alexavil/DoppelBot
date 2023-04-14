71304eee2037a3587ca0a835f3dee590f63beeda
Inspector Soviet <www.alex.avil@mail.ru>
2023-04-14 20:03:37 +0400

    play command rewrite

--- commands/play.js
+++ commands/play.js
@@ -2,19 +2,27 @@ const InvidJS = require("@invidjs/invid-js");
 const debug = require("../index");
 const common = require("../music");
 const sqlite3 = require("better-sqlite3");
+const { AudioPlayerStatus, createAudioResource } = require("@discordjs/voice");
 
 const masterqueue = new sqlite3("./data/queue.db");
 const settings = new sqlite3("./data/settings.db");
 module.exports = {
   name: "play",
+  aliases: ["p"],
   description: "Play a track",
   async execute(message, args) {
     const id = message.guild.id;
+    if (!message.member.voice.channel) {
+      return message.reply("You need to join a voice channel first!");
+    }
     if (debug.debug === true)
       console.log("[DEBUG] Starting playback for " + id + "...");
     if (!args[0]) {
       return message.reply("Provide a valid link!");
     }
+    let default_url = settings
+      .prepare(`SELECT * FROM guild_${id} WHERE option = 'default_instance'`)
+      .get().value;
     url = args[0];
     if (common.disallowedLinks.some((link) => url.startsWith(link))) {
       if (debug.debug === true)
@@ -37,25 +45,38 @@ module.exports = {
       );
       url = default_url + "/watch?v=" + url;
     }
+    common.endTimeout(id);
     if (url.includes("/watch?v=")) {
+      if (debug.debug === true)
+        console.log("[DEBUG] Validating " + url + "...");
       let fetched = await common.getVideo(url);
       let queuelength = masterqueue
         .prepare(`SELECT * FROM guild_${id}`)
         .all().length;
       if (fetched !== undefined) {
+        if (debug.debug === true)
+          console.log("[DEBUG] Adding " + url + " to the queue...");
         masterqueue
           .prepare(`INSERT INTO guild_${id} VALUES (?, ?, ?)`)
           .run(url, message.author.id, "false");
-        if (queuelength === 1) {
+        if (queuelength === 0) {
+          if (debug.debug === true)
+            console.log("[DEBUG] Downloading stream...");
           let stream = await InvidJS.fetchSource(
             fetched.instance,
             fetched.video,
             fetched.format,
             { saveTo: InvidJS.SaveSourceTo.Memory, parts: 10 }
           );
-          common.playMusic(message.member.voice.channel, stream);
+          if (debug.debug === true) console.log("[DEBUG] Creating player...");
           message.channel.send(
             `Now playing: ${fetched.url}\nRequested by <@!${message.author.id}>`
+          );
+          common.playMusic(
+            message.member.voice.channel,
+            message.channel,
+            stream,
+            fetched
           );
         } else {
           message.reply(`Added ${fetched.url} to the queue!`);
@@ -79,14 +100,29 @@ module.exports = {
         });
         message.reply("Playlist added to queue!");
         if (queuelength === 0) {
+          let first = masterqueue
+            .prepare(
+              `SELECT * FROM guild_${id} ORDER BY ROWID LIMIT 1`
+            )
+            .get();
+          let vid = await common.getVideo(first.track);
+          if (debug.debug === true)
+            console.log("[DEBUG] Downloading stream...");
+          let stream = await InvidJS.fetchSource(
+            vid.instance,
+            vid.video,
+            vid.format,
+            { saveTo: InvidJS.SaveSourceTo.Memory, parts: 10 }
+          );
+          if (debug.debug === true) console.log("[DEBUG] Creating player...");
+          message.channel.send(
+            `Now playing: ${vid.url}\nRequested by <@!${message.author.id}>`
+          );
           common.playMusic(
             message.member.voice.channel,
-            fetched.instance.url + "/watch?v=" + fetched.playlist.videos[0].id
-          );
-          message.channel.send(
-            `Now playing: ${
-              fetched.instance.url + "/watch?v=" + fetched.playlist.videos[0].id
-            }\nRequested by <@!${message.author.id}>`
+            message.channel,
+            stream,
+            vid
           );
         }
       }

d3981aa57f98ca7cb7ae953c238de7246d4b9db8
alexavil <www.alex.avil@mail.ru>
2024-01-12 22:46:26 +0400

    stats: add uptime and ping

--- buttons/stats.js
+++ buttons/stats.js
@@ -2,6 +2,7 @@ import Discord from "discord.js";
 import child from "child_process";
 const name = process.env.NAME;
 import os from "os";
+import { convertToString } from "../utils/TimeConverter.js";
 export default {
   name: "stats",
   async execute(interaction) {
@@ -13,21 +14,26 @@ export default {
         {
           name: "System Information",
           value: `
-            Commit: \`${commit}\`
-            OS: \`${os.type()} ${os.release} ${os.arch}\`
-            Node Version: \`${process.version}\`
-            Discord.js Version: \`${Discord.version}\`
-            Available RAM: \`${Math.round(
-              os.freemem() / 1024 / 1024,
-            )} / ${Math.round(os.totalmem() / 1024 / 1024)} MB\`,
-            CPU: \`${os.cpus()[0].model}\`
-            CPU Usage: \`${os.loadavg()[0].toFixed(2)}%\``,
+            \`\`\`js
+Commit: ${commit}
+OS: ${os.type()} ${os.release} ${os.arch}
+Node Version: ${process.version}
+Discord.js Version: ${Discord.version}
+Available RAM: ${Math.round(
+            os.freemem() / 1024 / 1024
+          )} / ${Math.round(os.totalmem() / 1024 / 1024)} MB,
+CPU: ${os.cpus()[0].model}
+CPU Usage: ${os.loadavg()[0].toFixed(2)}%
+DoppelBot Uptime: ${convertToString(process.uptime())}
+OS Uptime: ${convertToString(os.uptime())}\`\`\``,
         },
         {
           name: "Bot Stats",
-          value: `Total servers: \`${Array.from(interaction.client.guilds.cache).length}\`
-          Total users: \`${Array.from(interaction.client.users.cache).length}\``,
-        },
+          value: `\`\`\`js
+Total Servers: ${Array.from(interaction.client.guilds.cache).length}
+Total Users: ${Array.from(interaction.client.users.cache).length}
+WebSocket Ping: ${interaction.client.ws.ping}ms.\`\`\``,
+        }
       );
     interaction.reply({ embeds: [stats], ephemeral: true });
   },

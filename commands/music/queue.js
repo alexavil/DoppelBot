4866e25cbdc758949af04886cfa6321e33c94cf5
alexavil <alexavil007@gmail.com>
2025-03-21 19:01:15 +0000

    restore queue as it doesn't rely on any external functions

---
+++ commands/music/queue.js
@@ -0,0 +1,41 @@
+import Discord, { ButtonStyle } from "discord.js";
+const debug = process.env.DEBUG;
+import sqlite3 from "better-sqlite3";
+
+const queue = new sqlite3("./data/queue.db");
+export default {
+  data: new Discord.SlashCommandBuilder()
+    .setName("queue")
+    .setDescription("View current queue"),
+  async execute(interaction) {
+    const id = interaction.guild.id;
+    let counter = 0;
+    let embed = new Discord.EmbedBuilder();
+    let queuelength = queue
+      .prepare(`SELECT * FROM guild_${id}`)
+      .all().length;
+    if (queuelength !== 0) {
+      if (debug === "true")
+        console.log(`[DEBUG] Queue for ${id} is not empty, fetching tracks...`);
+      queue
+        .prepare(`SELECT * FROM guild_${id}`)
+        .all()
+        .forEach((track) => {
+          if (counter < 25) {
+            embed.addFields({
+              name: track.name,
+              value: `${track.track}\nRequested by: <@!${track.author}>`,
+              inline: true,
+            });
+            counter++;
+          }
+        });
+    } else {
+      if (debug === "true") console.log(`[DEBUG] Queue for ${id} is empty...`);
+      embed.setDescription("The queue is currently empty.");
+    }
+    embed.setTitle(`Queue for ${interaction.guild.name}`);
+    embed.setColor("#0099ff");
+    return interaction.editReply({ embeds: [embed] });
+  },
+};

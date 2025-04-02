1d57064c5e924787652092bf8ce33419fa29824d
alexavil <alexavil007@gmail.com>
2025-04-02 16:23:11 +0000

    fix deploy script

--- utils/deploy.js
+++ utils/deploy.js
@@ -15,7 +15,7 @@ const token = process.env.TOKEN;
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, '../interactions/commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
